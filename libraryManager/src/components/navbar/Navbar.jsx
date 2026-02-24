860f29e0453f20233c206fd6ad13b86ae2a7020f
Kalyan <Kalyan.Arutla@WinWire.com>
2026-02-24 17:47:36 +0530

    Completed Navbar component

--- libraryManager/src/components/navbar/Navbar.jsx
+++ libraryManager/src/components/navbar/Navbar.jsx
@@ -9,8 +9,8 @@ function Navbar(){
         <div className="nav-container">
             <div className="nav-left">
                 <img  className="logo-image" src={logo} alt = "temporary logo of the website"/>
-                <h3> Smart Library</h3>
-                <img className="notification-image" src = {notify} />
+                <span> Smart Library</span>
+                <img className="notification-image" src = {notify} alt="Notification button" onClick={()=>alert("Notification button is clicked")} />
             </div>
 
             <div className="nav-right">
